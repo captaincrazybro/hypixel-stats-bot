eb23f945baf70a9c4968442966da1d51d378d2fb
Glitch (hello-sqlite) <none>
2020-10-12 01:42:49 +0000

    🍁🐯 Checkpoint ./package.json:1671968/24 ./index.js:1671968/660

---
+++ index.js
@@ -0,0 +1,9 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'hypixel.net', // optional
+  port: 25565,       // optional
+  username: process.env.EMAIL, // email and password are required only for
+  password: process.env.PASSWORD,          // online-mode=true servers
+  version: false                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
+})
