c0cb34b03a62102d908fed73949495572568a508
Glitch (hello-sqlite) <none>
2020-10-14 17:17:42 +0000

    🏝☃️ Checkpoint ./index.js:1671968/157

--- index.js
+++ index.js
@@ -178,22 +178,27 @@ function getStats(sender, args){
   else player = args[1];
   
   hypixel.getPlayerByName(process.env.APIKEY, player).then(obj => {
+    console.log("hi");
     if(obj.player == null || !obj.success) return sendMessage(sender, "Invalid player");
     if(obj.player.stats == null) return sendMessage(sender, "The bot encountered a temporary problem, please try again");
     let stats = obj.player.stats[gamemode];
     //console.log(stats);
     if(stats == undefined) return sendMessage(sender, "Invalid gamemode");
+    //player = obj.player.playername;
     switch(gamemode){
       case("Duels"):{
+        console.log("duels");
         sendMessage(sender, `${player}'s ${gamemode} stats - WS: ${stats.current_winstreak}, Best WS: ${stats.best_overall_winstreak}, Wins: ${stats.wins}, Losses: ${stats.losses}, Kills: ${stats.kills}, Deaths: ${stats.deaths}, WLR: ${Number.parseFloat(stats.wins/stats.losses).toFixed(2)}, KDR: ${Number.parseFloat(stats.kills/stats.deaths).toFixed(2)}`);
         break;
       }
       case("Bedwars"):{
+        console.log("bedwars");
         sendMessage(sender, `${player}'s ${gamemode} stats - Level: ${obj.player.achievements.bedwars_level}, XP: ${stats.Experience}, WS: ${stats.winstreak}, Finals Kills: ${stats.final_kills_bedwars}, Final Deaths: ${stats.final_deaths_bedwars}, Kills: ${stats.kills_bedwars}, Deaths: ${stats.deaths_bedwars}, Wins: ${stats.wins_bedwars}, Losses: ${stats.losses_bedwars}` +
                    `, FKDR: ${Number.parseFloat(stats.final_kills_bedwars/stats.final_deaths_bedwars).toFixed(2)}, WLR: ${Number.parseFloat(stats.wins_bedwars/stats.losses_bedwars).toFixed(2)}`)
         break;
       }
       case("SkyWars"):{
+        console.log("skywars");
         sendMessage(sender, `${player}'s ${gamemode} stats - Level: ${obj.player.achievements.skywars_you_re_a_star}, XP: ${stats.skywars_experience}, ` +
         `Wins: ${stats.wins}, Losses: ${stats.losses}, ` +
         `Kills: ${stats.kills}, Deaths: ${stats.deaths}, ` +
