c57846c3f4ddecd38529cce29fd94384198a86b4
Glitch (hello-sqlite) <none>
2020-10-18 22:35:13 +0000

    🍛🎢 Checkpoint ./index.js:1671968/15

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ bot.on('message', function (messageJson) {
                         break;
                       }
                     }
-                    }, 1000);
+                    }, 1000 + i * 1000);
                     if(partyMembers.indexOf(val) == (partyMembers.length - 1)){
                       nextParty();
                     }
